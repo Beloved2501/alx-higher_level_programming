9d0fb3b81477f7b40cb5aee24280bb23a313859e
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-10-02 11:19:00 -0700

    check cycle in C

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,18 +8,19 @@
  **/
 int check_cycle(istint_t *list)
 {
-	listint_t *fast_ptr = list;
 	listint_t *slow_ptr = list;
+	listint_t *fast_ptr = list;
+
+	if (!list)
+		return (0);
 
 	while (slow_ptr && fast_ptr && fast_ptr->next)
 	{
 		slow_ptr = slow_ptr->next;
 		fast_ptr = fast_ptr->next->next;
-
-		if (slow_ptr = fast_ptr)
-		{
-			return 1;
-		}
+		if (slow_ptr == fast_ptr)
+			return (1);
 	}
+
 	return (0);
 }
