5a8f1b71789e9ead3579d9e97c22657ba5dae47d
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-10-03 09:04:00 -0700

    print hexadecimal

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-i = 0
-while i < 99:
-    print("{:d} 0x{:02X}".format(i, i), end=" ")
-    i += 1
+num = 0
+while num < 99:
+    print("{:d} = 0x{:02X}".format(num, num))
+    num += 1
